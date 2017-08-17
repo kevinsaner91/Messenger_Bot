3658e87f5d16e6a76a3b261ac5d74262f4d901bb
kevinsaner91 <kevin.saner91@hotmail.com>
2017-08-17 14:13:10 +0200

    hello world

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ app.post('/webhook/', function (req, res) {
 				console.log("welcome to chatbot")
 				continue
 			}
+			if (text.substring.includes("Info")){
+				sendTextMessage(sender, "All relevant Info")
+			}
 			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 		}
 		if (event.postback) {
